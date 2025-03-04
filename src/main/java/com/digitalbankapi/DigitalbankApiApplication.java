aa820386b0c07c7d621aa9090bd16485bac8a021
Thalita.ribeiro <sribeiro.thalita@gmail.com>
2025-03-04 19:22:33 -0300

    🎉 Initial commit

---
+++ src/main/java/com/digitalbankapi/DigitalbankApiApplication.java
@@ -0,0 +1,13 @@
+package com.digitalbankapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DigitalbankApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DigitalbankApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/digitalbankapi/DigitalbankApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.digitalbankapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DigitalbankApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
